6efdf063890976b8f42511a500580690182cfd0d
NbMiranda <nathanbmiranda@hotmail.com>
2024-06-17 19:00:51 -0300

    .

--- obj/Debug/net8.0/LojaMicroservice.AssemblyInfo.cs
+++ obj/Debug/net8.0/LojaMicroservice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LojaMicroservice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f266911b442a0af35ef9504161c527dc439c3d67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+190bdd48b0b0bac287f13e8fff2b35b5d7e5fdab")]
 [assembly: System.Reflection.AssemblyProductAttribute("LojaMicroservice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LojaMicroservice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
